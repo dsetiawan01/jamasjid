9f6be57eef99efa2d922d9d7608a247225ed5f4b
Dodi Setiawan <d.setiawann7@gmail.com>
2021-07-12 10:57:20 +0700

    change swipper css

--- src/boot/swiper.js
+++ src/boot/swiper.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 
 // import style (>= Swiper 6.x)
-import 'swiper/swiper-bundle.css'
+// import 'swiper/swiper-bundle.css'
+import 'swiper/css/swiper.css';
 Vue.use(VueAwesomeSwiper)
 
 export { VueAwesomeSwiper }
